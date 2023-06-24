8650eb9ce28107b0ba3a95f2b9f2efb16b803f99
Dheeraj-pal <dheeraj.pal19@gmail.com>
2023-06-24 23:39:06 +0530

    meeting cancelation working

--- routes/meeting.route.js
+++ routes/meeting.route.js
@@ -105,13 +105,6 @@ meetingRouter.delete("/:meetingId", async (req, res) => {
       return res.status(404).json({ message: "Meeting not found" });
     }
 
-    if (
-      meeting.doctorId.toString() !== req.body.doctorId &&
-      meeting.patientId.toString() !== req.body.patientId
-    ) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-
     await meetModel.findByIdAndDelete(meetingId);
 
     res.json({ message: "Meeting canceled successfully" });
