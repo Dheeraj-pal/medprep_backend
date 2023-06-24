24df4522d45393ba93f7bab5c2d07d18f7af1b85
Dheeraj-pal <dheeraj.pal19@gmail.com>
2023-06-25 02:08:18 +0530

    cors error  updated

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Welcome to MEDPREP");
